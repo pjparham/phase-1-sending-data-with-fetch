3d0aed52bc13ebf307e7b6c76b13c315e2d3c77b
Parker Parham <parkerparham@gmail.com>
2022-07-31 16:40:32 -0400

    commit tree d1370f636cd10c3813de852bc4acaba18618ec70

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+}
+
+
+const configurationObject = {
+    method: "Post",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(formData)
+};
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+// .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   })
+//   .catch(function (error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   });
+
+function submitData(inputName,inputEmail){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: `${inputName}`,
+            email: `${inputEmail}`,
+        }),
+    })
+    .then(function (response) {
+        return response.json() //response is files, json is function that converts it to json data
+    })
+    .then(function(user){
+        // console.log(user)
+        const main = document.createElement("p");
+        main.innerHTML = `${user.id}`
+        document.body.append(main)
+    })
+    .catch(function(error) {
+        console.log('here is the error', error.message)
+        document.body.innerHTML = error.message
+    })
+}
